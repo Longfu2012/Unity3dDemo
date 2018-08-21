851b4af7bbe5071939b2b907481697675685976e
1187722803@qq.com <1187722803@qq.com>
2018-08-21 14:27:46 +0800

    Unity3d通信demo提交

---
+++ Unity3dDemo/Assets/Script/CallAndroid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CallAndroid : MonoBehaviour {
+
+	AndroidJavaClass androidjavaClass;
+	AndroidJavaObject androidjavaObj;
+
+	// Use this for initialization
+	void Start () {
+		androidjavaClass = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		androidjavaObj = androidjavaClass.GetStatic<AndroidJavaObject> ("currentActivity");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public void callToast(){
+		if(null != androidjavaObj){
+			androidjavaObj.Call ("showToast");
+		}
+	}
+
+	public void callToast2(){
+		if(null != androidjavaObj){
+			androidjavaObj.Call ("showToast","unity3d Str");
+		}
+	}
+
+	public void callAndroidStr(){
+		if(null != androidjavaObj){
+			string rlt = androidjavaObj.Call<string> ("getAndroidStr");
+			gameObject.GetComponentInChildren<Text>().text = rlt;
+		}
+	}
+
+}
---
+++ Unity3dDemo/Assets/Script/CallUnity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CallUnity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeObj1(){
+		gameObject.GetComponentInChildren<Text>().text = "android调用 无参方法";
+	}
+
+	public void ChangeObj2(string str){
+		gameObject.GetComponentInChildren<Text>().text = "android调用 有参方法 参数:" + ((str == null ) ? "get null ?":str);
+	}
+
+}
